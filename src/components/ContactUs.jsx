75db8d26c546ff3c6692dd979a068a9a0ebaeb2f
Divesh Sharma <45972450+divesh9892@users.noreply.github.com>
2025-04-14 21:45:58 +0530

    added deployed url

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -66,7 +66,7 @@ const ContactUs = () => {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/v1/contact", // Use env variable
+        "https://jogire-backend.onrender.com/api/v1/contact",
         formData,
         { timeout: 10000 }
       );
